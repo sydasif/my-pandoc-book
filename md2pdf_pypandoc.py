c14b35348569b31b28e01afd8bb5b83bf4187210
Syed Asif <sydasif78@gmail.com>
2023-11-23 12:44:11 +0500

    Pypandoc script

---
+++ md2pdf_pypandoc.py
@@ -0,0 +1,36 @@
+import os
+import pypandoc
+
+input_dir = "chapters"
+output_dir = "my_book"
+output_filename = "book_output_02.pdf"
+
+# Ensure the output directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+# Build the pandoc options as a string
+pandoc_cmd = [
+    "--toc",
+    "--number-sections",
+    "--resource-path=.",
+    "--metadata-file", "metadata.yml",
+    "--include-in-header", "main.tex",
+    "--highlight-style", "pygments.theme",
+    "-V", "toc-title=Table of contents",
+    "-V", "linkcolor:blue",
+    "-V", "geometry:a4paper",
+    "-V", "geometry:margin=1.8cm",
+    "-V", "mainfont=DejaVu Serif",
+    "-V", "monofont=SauceCodePro Nerd Font",
+    "--pdf-engine=xelatex",
+]
+
+output_path = os.path.join(output_dir, output_filename)
+# Convert all markdown files in the chapters/ subdirectory.
+pypandoc.convert_file(
+    os.path.join(input_dir, "*.md"), "pdf",
+    outputfile=output_path,
+    extra_args=pandoc_cmd,
+)
+
+print(f"Conversion completed. Output saved to: {output_path}")
