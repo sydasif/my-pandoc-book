e430f38cce255d45828761c3ee79f875d185f8dd
Syed Asif <sydasif78@gmail.com>
2023-11-23 12:43:05 +0500

    format the script with black

--- md2pdf.py
+++ md2pdf.py
@@ -8,31 +8,34 @@ output_dir = "my_book"
 os.makedirs(output_dir, exist_ok=True)
 
 # List all files in the input directory and sort them numerically based on the prefix
-input_files = sorted([
-    os.path.join(input_dir, file) for file in os.listdir(input_dir)
-    if os.path.isfile(os.path.join(input_dir, file))
-], key=lambda x: int(os.path.splitext(os.path.basename(x).split("-")[0])[0]))
+input_files = sorted(
+    [
+        os.path.join(input_dir, file)
+        for file in os.listdir(input_dir)
+        if os.path.isfile(os.path.join(input_dir, file))
+    ],
+    key=lambda x: int(os.path.splitext(os.path.basename(x).split("-")[0])[0]),
+)
 
 # # Build the pandoc command
-# pandoc_cmd = [
-#     "pandoc",
-#     *input_files,
-#     "--toc",
-#     "--number-sections",
-#     "--resource-path=.",
-#     "--metadata-file", "metadata.yml",
-#     "--include-in-header", "main.tex",
-#     "--highlight-style", "pygments.theme",
-#     "-V", "toc-title=Table of contents",
-#     "-V", "linkcolor:blue",
-#     "-V", "geometry:a4paper",
-#     "-V", "geometry:margin=1.8cm",
-#     "-V", "mainfont=DejaVu Serif",
-#     "-V", "monofont=SauceCodePro Nerd Font",
-#     "--pdf-engine=xelatex",
-#     "-o", os.path.join(output_dir, "book_output.pdf")
-# ]
+pandoc_cmd = [
+    "pandoc",
+    *input_files,
+    "--toc",
+    "--number-sections",
+    "--resource-path=.",
+    "--metadata-file", "metadata.yml",
+    "--include-in-header", "main.tex",
+    "--highlight-style", "pygments.theme",
+    "-V", "toc-title=Table of contents",
+    "-V", "linkcolor:blue",
+    "-V", "geometry:a4paper",
+    "-V", "geometry:margin=1.8cm",
+    "-V", "mainfont=DejaVu Serif",
+    "-V", "monofont=SauceCodePro Nerd Font",
+    "--pdf-engine=xelatex", "-o", 
+    os.path.join(output_dir, "book_output.pdf"),
+]
 
-# # Run the pandoc command
-# subprocess.run(pandoc_cmd)
-print(input_files)
+# Run the pandoc command
+subprocess.run(pandoc_cmd)
