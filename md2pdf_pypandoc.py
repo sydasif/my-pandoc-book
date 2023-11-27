4e3f23c6256142a15c96495b729bae6ae0bc4f46
Syed Asif <sydasif78@gmail.com>
2023-11-27 18:58:05 +0500

    Add toc-depth

--- md2pdf_pypandoc.py
+++ md2pdf_pypandoc.py
@@ -11,20 +11,27 @@ os.makedirs(output_dir, exist_ok=True)
 # Build the pandoc options as a string
 pandoc_cmd = [
     "--table-of-contents",
+    "--toc-depth=2",
     "--number-sections",
     "--resource-path=.",
     "--pdf-engine=xelatex",
-    "--metadata-file", "metadata.yml",
-    "--include-in-header", "main.tex",
-    "--highlight-style", "tango",
-    "--include-before-body", "cover.tex",
-    "-V", "toc-title=Table of contents",
+    "--metadata-file",
+    "metadata.yml",
+    "--include-in-header",
+    "main.tex",
+    "--highlight-style",
+    "tango",
+    "--include-before-body",
+    "cover.tex",
+    "-V",
+    "toc-title=Table of contents",
 ]
 
 output_path = os.path.join(output_dir, output_filename)
 # Convert all markdown files in the chapters/ subdirectory.
 pypandoc.convert_file(
-    os.path.join(input_dir, "*.md"), "pdf",
+    os.path.join(input_dir, "*.md"),
+    "pdf",
     outputfile=output_path,
     extra_args=pandoc_cmd,
 )
